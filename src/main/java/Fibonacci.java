777adeb2304290f1af9d83d235a63688dff39f30
Ana <flavia.oliveira@ntconsult.com.br>
2022-02-07 10:47:32 -0300

    Correção exercicio Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -9,6 +9,7 @@ public class Fibonacci {
             System.out.println(numero);
         });
     }
+
     private static List<Integer> fibonacci () {
 
         ArrayList<Integer> lista = new ArrayList<>();
@@ -16,17 +17,10 @@ public class Fibonacci {
         int soma = 1;
         int somaAnterior = 0;
 
-        for (int n = 1; n < 300; n++){
-            if (soma + somaAnterior > 300 ){
-                break;
-            }
-            if ( n < 2 ){
-                lista.add(n);
-            }else {
-                soma = soma + somaAnterior;
-                somaAnterior = soma - somaAnterior;
-                lista.add(soma);
-            }
+        while (soma + somaAnterior < 300){
+            soma = soma + somaAnterior;
+            somaAnterior = soma - somaAnterior;
+            lista.add(soma);
         }
         return lista;
     }
